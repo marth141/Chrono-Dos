0a89ae0378a532e92ade144e3cd84c03abbccf02
Seriously <ncasados@live.com>
2018-04-20 15:36:24 -0600

    Added final column adder.

--- Project/addColumns.js
+++ Project/addColumns.js
@@ -1,15 +1,9 @@
-function addLastThreeColumns() {
-
-}
-
-function addAssignedColumn() {
-
-}
-
-function addPriorityColumn() {
-
-}
-
-function addStatusColumn() {
-    
+function addLastColumns(stagingBacklog) {
+  var dim = getDimensions(stagingBacklog);
+  var columnsToAdd = ['Assigned', 'Priority', 'Status'];
+  var toAddIndex = 0;
+  for (var end = dim[1] + 1; end <= dim[1] + 3; end++) {
+    stagingBacklog.getRange(0, end).setValue(columnsToAdd[toAddIndex]);
+    toAddIndex++;
+  }
 }

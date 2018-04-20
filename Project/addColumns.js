3ce32f56e16ad884231ec0635aa6d87c20a78f04
Seriously <ncasados@live.com>
2018-04-20 15:44:09 -0600

    Reworded addColumn function

--- Project/addColumns.js
+++ Project/addColumns.js
@@ -1,9 +1,11 @@
 function addLastColumns(stagingBacklog) {
   var dim = getDimensions(stagingBacklog);
+  var addColumnOrigin = dim[1] + 1;
+  var lastColToAdd = dim[1] + 3;
   var columnsToAdd = ['Assigned', 'Priority', 'Status'];
   var toAddIndex = 0;
-  for (var end = dim[1] + 1; end <= dim[1] + 3; end++) {
-    stagingBacklog.getRange(0, end).setValue(columnsToAdd[toAddIndex]);
+  for (var addHere = addColumnOrigin; addHere <= lastColToAdd; addHere++) {
+    stagingBacklog.getRange(0, addHere).setValue(columnsToAdd[toAddIndex]);
     toAddIndex++;
   }
 }

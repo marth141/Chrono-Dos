b67e0e29d916df0427c2420bb19abccc3afd506c
marth141 <ncasados@live.com>
2018-04-26 08:50:23 -0600

    Revising lastColumnToAdd

--- Project/universal/universal-AddAssignedPriorityStatusCol.js
+++ Project/universal/universal-AddAssignedPriorityStatusCol.js
@@ -24,7 +24,7 @@ function uni_addLastColumns(stagingBacklog) {
   var dim = getDimensions(stagingBacklog);
   var columnsToAdd = ['Assigned', 'Priority', 'Status', 'Notes', 'Last Update'];
   var addColumnOrigin = dim[1] + 1;
-  var lastColToAdd = dim[1] + columnsToAdd.length + 1;
+  var lastColToAdd = dim[1] + columnsToAdd.length;
   var toAddIndex = 0;
   for (var addHere = addColumnOrigin; addHere <= lastColToAdd; addHere++) {
     stagingBacklog.getRange(1, addHere).setValue(columnsToAdd[toAddIndex]);

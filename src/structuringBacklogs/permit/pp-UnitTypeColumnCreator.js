fd04d52b2df0d5a10d464299adfd08cd8685cef4
marth141 <ncasados@live.com>
2018-08-11 14:34:50 -0600

    Updated UnitTypeColCreator
    Updated this for better notes and structure.

--- src/structuringBacklogs/permit/pp-UnitTypeColumnCreator.js
+++ src/structuringBacklogs/permit/pp-UnitTypeColumnCreator.js
@@ -1,142 +1,134 @@
-// Will need edit to check states.
+/**
+ * @param {Array} backlogArray
+ * @return {Array} backlogArray
+ */
+function pp_UnitTypeMarker(backlogArray) {
+  var columns = {
+    officeCol: getMeThatColumn(
+      'Service: Regional Operating Center',
+      backlogArray
+    ),
+    opporTypeCol: getMeThatColumn('Opportunity: Type', backlogArray),
+    dueDateCol: getMeThatColumn('DUE DATE', backlogArray),
+    assignCol: undgetMeThatColumn(
+      'Phase: PV Design Completed By',
+      backlogArray
+    ),
+    primaryDateCol: getMeThatColumn(
+      'Primary CAD: Permit Packet QA Completed',
+      backlogArray
+    ),
+    srNeededCol: getMeThatColumn(
+      'Phase: Structural Review Completed',
+      backlogArray
+    )
+  };
 
-/* exported
-debugPropUnitType
-*/
-
-/* global
-ServiceMasterBacklog
-getMeThatColumn
-ServiceOfficeCollection
-*/
-
-function debugPermitUnitType() {
-  var masterBacklogs = new ServiceMasterBacklog();
-  prop_UnitTypeMarker(masterBacklogs.Collection);
-  return;
+  backlogArray = pp_MarkUnits(backlogArray, columns);
+  return backlogArray;
 }
 
 /**
- *
- *
- * @param {any} propBacklog
- * @returns
+ * @param {Array} backlogArray
+ * @param {Object} columns
+ * @return {Array} backlogArray
  */
-function pp_UnitTypeMarker(backlogArray) {
-  var officeCol = getMeThatColumn(
-    "Service: Regional Operating Center",
-    backlogArray
-  );
-  var opporTypeCol = getMeThatColumn("Opportunity: Type", backlogArray);
-  var dueDateCol = getMeThatColumn("DUE DATE", backlogArray);
-  var assignCol = getMeThatColumn(
-    "Phase: PV Design Completed By",
-    backlogArray
-  );
-
-  var primaryDateCol = getMeThatColumn(
-    "Primary CAD: Permit Packet QA Completed",
-    backlogArray
-  );
-  var srNeededCol = getMeThatColumn(
-    "Phase: Structural Review Completed",
-    backlogArray
-  );
-
-  backlogArray = pp_MarkUnits(
-    backlogArray,
+function pp_MarkUnits(backlogArray, columns) {
+  var {
     officeCol,
     opporTypeCol,
     dueDateCol,
     assignCol,
     primaryDateCol,
     srNeededCol
-  );
-  return backlogArray;
-}
+  } = columns;
+  var backlogArrayHeaders = backlogArray[0];
 
-/**
- *
- *
- * @param {array} backlogArray
- * @param {number} designPathCol
- * @param {number} opporTypeCol
- * @param {array} dim
- * @returns
- */
-function pp_MarkUnits(
-  backlogArray,
-  officeCol,
-  opporTypeCol,
-  dueDateCol,
-  assignCol,
-  primaryDateCol,
-  srNeededCol
-) {
-  // Add Unit Type Column before Opportunity: Type Column
-  backlogArray[0].splice(opporTypeCol, 0, "UNIT TYPE");
+  // Add Unit Type Column before Opportunity Type Column
+  backlogArrayHeaders.splice(opporTypeCol, 0, 'UNIT TYPE');
   var designPathString;
   for (var row = 1; row < backlogArray.length; row++) {
     var account = backlogArray[row];
-    if ("S-5920785" === backlogArray[row][0]) {
-      var x = 0;
-    }
 
-    // If the opportunity type is add-on, or the offices are az, ny-09, or il
-    // Then set design path string as permit
-    // All else set as outsource
-    // Then set design path string as outsource
+    /**
+     * * Description of upcoming block *
+     * Checks account unit type for
+     * add-on, or arizona, or ny-09,
+     * or illinois, or ca-02.
+     *
+     * If it is any of that,
+     * designPathString will be permit.
+     *
+     * If it is a new installation,
+     * designPathString will be outsource.
+     * The outsource may become SR if it qualifies.
+     *
+     * Duggers are added for convenience.
+     */
     if (
-      backlogArray[row][opporTypeCol].match(/add/i) ||
-      backlogArray[row][officeCol].match(/az-/i) ||
-      backlogArray[row][officeCol].match(/ny-09/i) ||
-      backlogArray[row][officeCol].match(/il-/i) ||
-      backlogArray[row][officeCol].match(/ca-02/i)
+      account[opporTypeCol].match(/add/i) ||
+      account[officeCol].match(/az-/i) ||
+      account[officeCol].match(/ny-09/i) ||
+      account[officeCol].match(/il-/i) ||
+      account[officeCol].match(/ca-02/i)
     ) {
-      // For debugging a specific service number.
-      // if (backlogArray[row][0].match(/S-5958052/)) {
-      //   console.error("Here it is @ Permit!");
-      //   console.info(backlogArray[row]);
-      // }
-      
-      designPathString = "PERMIT";
-    } else if (backlogArray[row][opporTypeCol].match(/new/i)) {
-      // For debugging a specific service number.
-      // if (backlogArray[row][0].match(/S-5958052/)) {
-      //   console.error("Here it is @ Outsource!");
-      //   console.info(backlogArray[row]);
-      // }
-      
-      // For debugging a specific service number.
-      if (backlogArray[row][opporTypeCol].match(/add/i)) {
-        console.error("Add-on passed into Outsource");
-        console.error(backlogArray[row]);
-        MailApp.sendEmail(
-          "nathan.casados@vivintsolar.com",
-          "Chrono Error found - Add-on passed to Outsource",
-          backlogArray[row] +
-            "\r\nWas found being passed to outsource in error."
-        );
-        throw "Add-on becoming outsource. Please report this issue.";
+      designPathString = 'PERMIT';
+
+      /**
+       * !For debugging service number in permit
+       */
+      if (account[0].match(/S-5958052/)) {
+        console.error('Here it is @ Permit!');
+        console.info(account);
       }
-      designPathString = "OUTSOURCE";
+    } else if (account[opporTypeCol].match(/new/i)) {
+      designPathString = 'OUTSOURCE';
+
+      /**
+       * Test if outsource should be SR
+       */
       if (
-        testDate(backlogArray[row][primaryDateCol]) &&
-        !testDate(backlogArray[row][srNeededCol])
+        testDate(account[primaryDateCol]) &&
+        !testDate(account[srNeededCol])
       ) {
-        // For debugging a specific service number.
-        // if (backlogArray[row][0].match(/S-5958052/)) {
-        //   console.error("Here it is @ SR!");
-        //   console.info(backlogArray[row]);
-        // }
-        designPathString = "SR";
+        designPathString = 'SR';
+
+        /**
+         * !For debugging service number in SR
+         */
+        if (account[0].match(/S-5958052/)) {
+          console.error('Here it is @ SR!');
+          console.info(account);
+        }
+      }
+
+      /**
+       * !For debugging if outsource is actually addon
+       * !Will email Nathan Casados if so.
+       */
+      if (account[opporTypeCol].match(/add/i)) {
+        console.error('Add-on passed into Outsource');
+        console.error(account);
+        MailApp.sendEmail(
+          'nathan.casados@vivintsolar.com',
+          'Chrono Error found - Add-on passed to Outsource',
+          account + '\r\nWas found being passed to outsource in error.'
+        );
+        var unitTypeMarkingError =
+          'Add-on becoming outsource. Please report this issue.';
+        throw unitTypeMarkingError;
       }
     }
-    backlogArray[row].splice(opporTypeCol, 0, designPathString);
+    account.splice(opporTypeCol, 0, designPathString);
   }
   return backlogArray;
 }
 
+/**
+ * @param {Date} date
+ * @return {Boolean} result
+ */
 function testDate(date) {
-  return date instanceof Date;
+  var result = date instanceof Date;
+  return result;
 }

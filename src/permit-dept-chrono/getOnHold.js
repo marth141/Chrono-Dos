046939faa568cb4ceb38a56e8959b65ac789100f
marth141 <ncasados@live.com>
2018-08-29 18:39:07 -0600

    Setting dept as dev and formatting

--- src/permit-dept-chrono/getOnHold.js
+++ src/permit-dept-chrono/getOnHold.js
@@ -1,24 +1,34 @@
+/**
+ * Gets the on hold accounts
+ */
 function getOnHold() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var ssOnHold = ss.getSheetByName("On Hold");
+  var ssOnHold = ss.getSheetByName('On Hold');
   // Dev: 1r06cw7MtVKolZY6pXkmuoxcWPUeqtdm8Tu9sc5ljBkg
   // Prod: 121UKskNpiVK2ocT8pFIx9uO6suw3o7S7C4VhiIaqzI0
-  var ssReport = SpreadsheetApp.openById("121UKskNpiVK2ocT8pFIx9uO6suw3o7S7C4VhiIaqzI0").getSheetByName("Report");
+  var ssReport = SpreadsheetApp.openById(
+    '1r06cw7MtVKolZY6pXkmuoxcWPUeqtdm8Tu9sc5ljBkg'
+  ).getSheetByName('Report');
   var completeBacklog = [];
-  
-  var header = ssReport.getRange("2:2").getValues()[0];
-  var dueInCol = header.indexOf("DUE IN: (hh:mm)");
-  var officeCol = header.indexOf("OFFICE") -dueInCol;
-  var statusCol = header.indexOf("STATUS") -dueInCol;
-  var redesignCol = header.indexOf("REDESIGN ASSIGNMENT") -dueInCol;
-  var lastRow = ssReport.getLastRow()-1;
+
+  var header = ssReport.getRange('2:2').getValues()[0];
+  var dueInCol = header.indexOf('DUE IN: (hh:mm)');
+  var officeCol = header.indexOf('OFFICE') - dueInCol;
+  var statusCol = header.indexOf('STATUS') - dueInCol;
+  var redesignCol = header.indexOf('REDESIGN ASSIGNMENT') - dueInCol;
+  var lastRow = ssReport.getLastRow() - 1;
   var test = redesignCol - dueInCol;
-  completeBacklog = ssReport.getRange(3, dueInCol+1, lastRow, redesignCol+1).getValues().filter(function(row) { return row[statusCol].match(/on hold/i) });  // 3,dueInCol,stageCol,lastRow
-  
-  lastRow = ssOnHold.getLastRow()-1;
+  completeBacklog = ssReport
+    .getRange(3, dueInCol + 1, lastRow, redesignCol + 1)
+    .getValues()
+    .filter(function(row) {
+      return row[statusCol].match(/on hold/i);
+    }); // 3,dueInCol,stageCol,lastRow
+
+  lastRow = ssOnHold.getLastRow() - 1;
   var lastCol = ssOnHold.getLastColumn();
-  ssOnHold.getRange(3, 4, lastRow, lastCol-3).clearContent();
-    
+  ssOnHold.getRange(3, 4, lastRow, lastCol - 3).clearContent();
+
   var rowNeeded = completeBacklog.length;
   if (rowNeeded > 0) {
     var colNeeded = completeBacklog[0].length;

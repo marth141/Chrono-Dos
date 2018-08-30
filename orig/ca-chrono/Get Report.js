03d6f519ad7152b13fd1995b9054a9b33ce4124f
marth141 <ncasados@live.com>
2018-08-29 19:04:12 -0600

    Updating orig folder with src

--- orig/ca-chrono/Get Report.js
+++ orig/ca-chrono/Get Report.js
@@ -1,35 +1,43 @@
+/**
+ * Get's the report from the master chrono
+ */
 function getReport() {
-  var ssThis = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Report");
-  var ssReport = SpreadsheetApp.openById("121UKskNpiVK2ocT8pFIx9uO6suw3o7S7C4VhiIaqzI0").getSheetByName("Report");
+  var ssThis = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Report');
+  // Dev: 1r06cw7MtVKolZY6pXkmuoxcWPUeqtdm8Tu9sc5ljBkg
+  // Prod: 121UKskNpiVK2ocT8pFIx9uO6suw3o7S7C4VhiIaqzI0
+  var ssReport = SpreadsheetApp.openById(
+    '1r06cw7MtVKolZY6pXkmuoxcWPUeqtdm8Tu9sc5ljBkg'
+  ).getSheetByName('Report');
   var completeBacklog = [];
-  
-  var header = ssReport.getRange("2:2").getValues()[0];
-  var dueInCol = header.indexOf("DUE IN: (hh:mm)");
-  var solarProjectCol = header.indexOf("SOLAR PROJECT") -dueInCol;
-  var cadObjCol = header.indexOf("CAD OBJECT") -dueInCol;
-  var officeCol = header.indexOf("OFFICE") -dueInCol;
-  var statusCol = header.indexOf("STATUS") -dueInCol;
-  var redesignCol = header.indexOf("REDESIGN ASSIGNMENT") -dueInCol;
-  var lastRow = ssReport.getLastRow()-1;
-  var range = ssReport.getRange(3, dueInCol+1, lastRow, redesignCol+1);
+
+  var header = ssReport.getRange('2:2').getValues()[0];
+  var dueInCol = header.indexOf('DUE IN: (hh:mm)');
+  var solarProjectCol = header.indexOf('SOLAR PROJECT') - dueInCol;
+  var cadObjCol = header.indexOf('CAD OBJECT') - dueInCol;
+  var officeCol = header.indexOf('OFFICE') - dueInCol;
+  var statusCol = header.indexOf('STATUS') - dueInCol;
+  var redesignCol = header.indexOf('REDESIGN ASSIGNMENT') - dueInCol;
+  var lastRow = ssReport.getLastRow() - 1;
+  var range = ssReport.getRange(3, dueInCol + 1, lastRow, redesignCol + 1);
   var formulas = range.getFormulas();
-  completeBacklog = range.getValues().filter(function(row,index) { 
-    var foundStateBool = ["ca-"].some(function(state) { return row[officeCol].toLowerCase().indexOf(state) > -1 });
+  completeBacklog = range.getValues().filter(function(row, index) {
+    var foundStateBool = ['ca-'].some(function(state) {
+      return row[officeCol].toLowerCase().indexOf(state) > -1;
+    });
     var onHoldBool = row[statusCol].match(/on hold/i);
-    if(foundStateBool && !onHoldBool) {
+    if (foundStateBool && !onHoldBool) {
       row[solarProjectCol] = formulas[index][solarProjectCol];
       row[cadObjCol] = formulas[index][cadObjCol];
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   });
-  
-  lastRow = ssThis.getLastRow()-1;
+
+  lastRow = ssThis.getLastRow() - 1;
   var lastCol = ssThis.getLastColumn();
-  ssThis.getRange(3, 4, lastRow, lastCol-3).clearContent();
-    
+  ssThis.getRange(3, 4, lastRow, lastCol - 3).clearContent();
+
   var rowNeeded = completeBacklog.length;
   if (rowNeeded > 0) {
     var colNeeded = completeBacklog[0].length;
--- orig/ca-chrono/On Hold.js
+++ orig/ca-chrono/On Hold.js
@@ -1,33 +1,41 @@
+/**
+ * Gets the on hold accounts
+ */
 function getOnHold() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var ssOnHold = ss.getSheetByName("On Hold");
-  var ssReport = SpreadsheetApp.openById("121UKskNpiVK2ocT8pFIx9uO6suw3o7S7C4VhiIaqzI0").getSheetByName("Report");
+  var ssOnHold = ss.getSheetByName('On Hold');
+  // Dev: 1r06cw7MtVKolZY6pXkmuoxcWPUeqtdm8Tu9sc5ljBkg
+  // Prod: 121UKskNpiVK2ocT8pFIx9uO6suw3o7S7C4VhiIaqzI0
+  var ssReport = SpreadsheetApp.openById(
+    '1r06cw7MtVKolZY6pXkmuoxcWPUeqtdm8Tu9sc5ljBkg'
+  ).getSheetByName('Report');
   var completeBacklog = [];
-  
-  var header = ssReport.getRange("2:2").getValues()[0];
-  var dueInCol = header.indexOf("DUE IN: (hh:mm)");
-  var solarProjectCol = header.indexOf("SOLAR PROJECT") -dueInCol;
-  var officeCol = header.indexOf("OFFICE") -dueInCol;
-  var statusCol = header.indexOf("STATUS") -dueInCol;
-  var redesignCol = header.indexOf("REDESIGN ASSIGNMENT") -dueInCol;
-  var lastRow = ssReport.getLastRow()-1;
-  var range = ssReport.getRange(3, dueInCol+1, lastRow, redesignCol+1);
+
+  var header = ssReport.getRange('2:2').getValues()[0];
+  var dueInCol = header.indexOf('DUE IN: (hh:mm)');
+  var solarProjectCol = header.indexOf('SOLAR PROJECT') - dueInCol;
+  var officeCol = header.indexOf('OFFICE') - dueInCol;
+  var statusCol = header.indexOf('STATUS') - dueInCol;
+  var redesignCol = header.indexOf('REDESIGN ASSIGNMENT') - dueInCol;
+  var lastRow = ssReport.getLastRow() - 1;
+  var range = ssReport.getRange(3, dueInCol + 1, lastRow, redesignCol + 1);
   var formulas = range.getFormulas();
-  completeBacklog = range.getValues().filter(function(row, index) { 
-    var foundState = ["ca-"].some(function(state) { return row[officeCol].toLowerCase().indexOf(state) > -1 });
-    if(!foundState) {
+  completeBacklog = range.getValues().filter(function(row, index) {
+    var foundState = ['ca-'].some(function(state) {
+      return row[officeCol].toLowerCase().indexOf(state) > -1;
+    });
+    if (!foundState) {
       return false;
-    }
-    else {
+    } else {
       row[solarProjectCol] = formulas[index][solarProjectCol];
       return row[statusCol].match(/on hold/i);
     }
-  });  // 3,dueInCol,stageCol,lastRow
-  
-  lastRow = ssOnHold.getLastRow()-1;
+  }); // 3,dueInCol,stageCol,lastRow
+
+  lastRow = ssOnHold.getLastRow() - 1;
   var lastCol = ssOnHold.getLastColumn();
-  ssOnHold.getRange(3, 4, lastRow, lastCol-3).clearContent();
-    
+  ssOnHold.getRange(3, 4, lastRow, lastCol - 3).clearContent();
+
   var rowNeeded = completeBacklog.length;
   if (rowNeeded > 0) {
     var colNeeded = completeBacklog[0].length;

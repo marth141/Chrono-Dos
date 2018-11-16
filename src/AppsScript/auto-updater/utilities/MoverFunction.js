81dba52fdbad6f7a3ea6f1f528aa609332677445
Nathan Casados <nathan.casados@gosolarit.com>
2018-11-16 11:39:45 -0700

    Updating with stuff from master

--- src/AppsScript/auto-updater/utilities/MoverFunction.js
+++ src/AppsScript/auto-updater/utilities/MoverFunction.js
@@ -12,24 +12,11 @@ function move_ToDestination() {
     .setValues(dimensionsJSON.source.data);
   SpreadsheetApp.flush();
   console.log('Updated ' + updateJSON.identity.team + ' ' + updateJSON.backlog2Update.getName());
-
-  // Try to update dev backlog
-  try {
-    updateJSON.dev2Update
-      .getRange(
-        1,
-        1,
-        dimensionsJSON.source.arrayDimensions[0],
-        dimensionsJSON.source.arrayDimensions[1]
-      )
-      .setValues(dimensionsJSON.source.data);
-    SpreadsheetApp.flush();
-    console.log('Updated ' + updateJSON.identity.team + ' ' + updateJSON.backlog2Update.getName());
-  } catch (e) {
-    console.log('No dev to move');
-  }
 }
 
+/**
+ * Moves for QCD
+ */
 function qcd_Move() {
   updateJSON.backlog2Update
     .getRange(
---
+++ src/AppsScript/proposal-qcd-form/README.md
@@ -0,0 +1,3 @@
+# Proposal QCD Form
+## Main Sheet
+https://docs.google.com/spreadsheets/d/1hXE0l7-FuK7wv83C_lRSVkmGPSgFtyz2wooeyEgVQxw/

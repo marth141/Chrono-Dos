0e7b84d72e5cc41e18c158c4a3e059e70edb5c2b
marth141 <ncasados@live.com>
2019-05-14 11:51:45 -0600

    Updating ballast tool

--- src/AppsScript/ballast-calculator/scripts/Code.js
+++ src/AppsScript/ballast-calculator/scripts/Code.js
@@ -1,20 +1,20 @@
+var exports = exports || {};
+var module = module || { exports: exports };
 function doGet() {
-  return HtmlService.createTemplateFromFile("index")
-    .evaluate()
-    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
+    return HtmlService.createTemplateFromFile("index")
+        .evaluate()
+        .setSandboxMode(HtmlService.SandboxMode.IFRAME);
 }
-
 function include(filename) {
-  return HtmlService.createHtmlOutputFromFile(filename)
-    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
-    .getContent();
+    return HtmlService.createHtmlOutputFromFile(filename)
+        .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+        .getContent();
 }
-
 function searchContent(template, callbackname) {
-  var res = new Array();
-  res[0] = HtmlService.createTemplateFromFile(template).getRawContent();
-  if (callbackname !== null) {
-    res[1] = callbackname;
-  }
-  return res;
+    var res = new Array();
+    res[0] = HtmlService.createTemplateFromFile(template).getRawContent();
+    if (callbackname !== null) {
+        res[1] = callbackname;
+    }
+    return res;
 }
--- src/AppsScript/ballast-calculator/scripts/ViabilityLogic.js
+++ src/AppsScript/ballast-calculator/scripts/ViabilityLogic.js
@@ -1,47 +1,52 @@
+var exports = exports || {};
+var module = module || { exports: exports };
 function stateTest(state) {
-  var approvedBallastRegions = [
-    "CA", "AZ", "NV", "NM", "TX",
-    "SC", "VA", "NJ", "PA", "UT"
-  ];
-
-  if (approvedBallastRegions.indexOf(state) !== -1) {
-    console.info(state + " Passes State Test");
-    return true;
-  } else {
-    console.info(state + " Failed State Test");
-    return false;
-  }
+    var approvedBallastRegions = [
+        "CA", "AZ", "NV", "NM", "TX",
+        "SC", "VA", "NJ", "PA", "UT"
+    ];
+    if (approvedBallastRegions.indexOf(state) !== -1) {
+        console.info(state + " Passes State Test");
+        return true;
+    }
+    else {
+        console.info(state + " Failed State Test");
+        return false;
+    }
 }
-
 function sdcSlopeTest(slope, sdc) {
-  if (sdc === "E" || sdc === "F") {
-    console.info("E and F Seismic Design Category not allowed.");
-    return 1;
-  } else if (sdc === "A" || sdc === "B") {
-    if (slope < 1.2 || slope > 7) {
-      console.info("Slope AB: " + slope);
-      return 2;
-    } else if (slope >= 1.2 || slope <= 7) {
-      console.info("Passes SDC and Slope Test");
-      return 0;
+    if (sdc === "E" || sdc === "F") {
+        console.info("E and F Seismic Design Category not allowed.");
+        return 1;
     }
-  } else if (sdc === "C" || sdc === "D") {
-    if (slope < 1.2 || slope > 3) {
-      console.info("Slope CD: " + slope);
-      return 3;
-    } else if (slope >= 1.2 || slope <= 7) {
-      console.info("Passes SDC and Slope Test");
-      return 0;
+    else if (sdc === "A" || sdc === "B") {
+        if (slope < 1.2 || slope > 7) {
+            console.info("Slope AB: " + slope);
+            return 2;
+        }
+        else if (slope >= 1.2 || slope <= 7) {
+            console.info("Passes SDC and Slope Test");
+            return 0;
+        }
     }
-  }
+    else if (sdc === "C" || sdc === "D") {
+        if (slope < 1.2 || slope > 3) {
+            console.info("Slope CD: " + slope);
+            return 3;
+        }
+        else if (slope >= 1.2 || slope <= 7) {
+            console.info("Passes SDC and Slope Test");
+            return 0;
+        }
+    }
 }
-
 function calculate_mpv(sdc, sds) {
-  if (sdc === "A" || sdc === "B") {
-    return 6;
-  } else {
-    var mpv = ((sds - 0.4) * (sds - 0.4)) * 60;
-    console.info("MPV: " + mpv);
-    return mpv;
-  }
+    if (sdc === "A" || sdc === "B") {
+        return 6;
+    }
+    else {
+        var mpv = ((sds - 0.4) * (sds - 0.4)) * 60;
+        console.info("MPV: " + mpv);
+        return mpv;
+    }
 }
